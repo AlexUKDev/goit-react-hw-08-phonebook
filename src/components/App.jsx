fd25ae8c0fec6a54866638077770c7f187f6f169
AlexUKDev <61418792+AlexUKDev@users.noreply.github.com>
2022-10-29 22:22:45 +0300

    Update App.jsx
    adds App on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { SectionTitle } from './SectionTitle/SectionTitle';
 import { Filter } from './Filter/Filter';
@@ -7,55 +7,55 @@ import { Notify } from 'notiflix';
 import { ContactsList } from './ContactList/ContactList';
 
 const LS_KEY = 'saved_contacts';
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     let parsedContacts = [];
 
     if (localStorage.getItem(LS_KEY)) {
       parsedContacts = JSON.parse(localStorage.getItem(LS_KEY));
     }
     if (parsedContacts.length !== 0) {
-      this.setState({ contacts: parsedContacts });
+      setContacts(parsedContacts);
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state) {
-      const prepareContacts = JSON.stringify(this.state.contacts);
-      localStorage.setItem(LS_KEY, prepareContacts);
-    }
-  }
+  useEffect(
+    prevContacts => {
+      if (prevContacts !== contacts) {
+        const prepareContacts = JSON.stringify(contacts);
+        localStorage.setItem(LS_KEY, prepareContacts);
+      }
+    },
+    [contacts]
+  );
 
-  addNewContact = newContact => {
+  const addNewContact = newContact => {
     // Сhecking if there is a contact with that name in the state
-    for (const contactItem of this.state.contacts) {
+    for (const contactItem of contacts) {
       if (newContact.name === contactItem.name) {
         return Notify.warning(`${newContact.name}, is alredy in contacts!'`);
       }
     }
     //Adding a new contact to the state
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+    setContacts(prevContacts => [...prevContacts, newContact]);
+
     Notify.success(`Contact ${newContact.name}, successfully added`);
   };
 
-  changeFilterValue = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilterValue = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  getFiltredContacts = () => {
-    const { filter, contacts } = this.state;
+  const getFiltredContacts = () => {
+    // const { filter, contacts } = this.state;
     const normalizeFilterValue = filter.toLowerCase();
 
     return contacts.filter(contact =>
@@ -63,40 +63,35 @@ export class App extends Component {
     );
   };
 
-  contactDelete = contactId => {
-    this.setState(prevState => ({
+  const contactDelete = contactId => {
+    setContacts(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
 
-  render() {
-    const visibleContacts = this.getFiltredContacts();
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          fontSize: 30,
-          color: '#010101',
-        }}
-      >
-        <SectionTitle title={'Phonebook'} />
-        <ContactForm sendNewContact={this.addNewContact} />
+  const visibleContacts = getFiltredContacts();
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontSize: 30,
+        color: '#010101',
+      }}
+    >
+      <SectionTitle title={'Phonebook'} />
+      <ContactForm sendNewContact={addNewContact} />
 
-        <SectionTitle title={'Contacts'} />
-        <Filter
-          lableText={'Find contacts by name'}
-          value={this.state.filter}
-          onChange={this.changeFilterValue}
-        />
+      <SectionTitle title={'Contacts'} />
+      <Filter
+        lableText={'Find contacts by name'}
+        value={filter}
+        onChange={changeFilterValue}
+      />
 
-        <ContactsList
-          contacts={visibleContacts}
-          contactDelete={this.contactDelete}
-        />
-      </div>
-    );
-  }
-}
+      <ContactsList contacts={visibleContacts} contactDelete={contactDelete} />
+    </div>
+  );
+};
