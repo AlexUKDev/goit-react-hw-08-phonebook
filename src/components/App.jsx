60b3f2a80eaf399611053a1a96fb4d68f2c83605
AlexUKDev <61418792+AlexUKDev@users.noreply.github.com>
2022-10-29 22:28:50 +0300

    bag fixed LS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,24 +8,26 @@ import { ContactsList } from './ContactList/ContactList';
 
 const LS_KEY = 'saved_contacts';
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LS_KEY)) ?? [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
+  );
   const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    let parsedContacts = [];
+  // useEffect(() => {
+  //   let parsedContacts = [];
 
-    if (localStorage.getItem(LS_KEY)) {
-      parsedContacts = JSON.parse(localStorage.getItem(LS_KEY));
-    }
-    if (parsedContacts.length !== 0) {
-      setContacts(parsedContacts);
-    }
-  }, []);
+  //   if (localStorage.getItem(LS_KEY)) {
+  //     parsedContacts = JSON.parse(localStorage.getItem(LS_KEY));
+  //   }
+  //   if (parsedContacts.length !== 0) {
+  //     setContacts(parsedContacts);
+  //   }
+  // }, []);
 
   useEffect(
     prevContacts => {
